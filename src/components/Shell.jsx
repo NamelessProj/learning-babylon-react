dba3e920c55c5595aa42d52daa856a7db0bdb5d6
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-05-28 16:30:49 +0200

    feat: Add Shell component with GameObjectContext and controllers

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,13 +1,16 @@
-// Shell.js
+// Shell.jsx
+import { GameObjectContext, GameObject } from '../contexts/GameObjectContext';
+import CameraController from './CameraController';
+import EnvironmentController from './EnvironnementController';
 
-const Shell = () => {
-
-  console.log('Shell');
-
+const Shell = ({ scene, engine }) => {
   return (
-    <>
-
-    </>
+    <GameObjectContext.Provider value={{ scene, engine }}>
+      <GameObject scene={scene} engine={engine}>
+        <CameraController />
+        <EnvironmentController />
+      </GameObject>
+    </GameObjectContext.Provider>
   );
 };
 

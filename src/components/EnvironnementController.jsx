8db54e558cc87b58d4a5efdca16e9a5f1df8edc9
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-05-28 16:38:14 +0200

    feat: Add sunlight to the scene

--- src/components/EnvironnementController.jsx
+++ src/components/EnvironnementController.jsx
@@ -1,6 +1,6 @@
 // EnvironmentController.jsx
 import React, { useEffect } from 'react';
-import { MeshBuilder } from '@babylonjs/core';
+import { MeshBuilder, HemisphericLight, Vector3 } from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 
 const EnvironmentController = () => {
@@ -9,6 +9,7 @@ const EnvironmentController = () => {
   useEffect(() => {
     if (scene) {
       createGround(scene);
+    createSunlight(scene);
     }
   }, [scene]);
 
@@ -18,6 +19,15 @@ const EnvironmentController = () => {
       { width: 10, height: 10 },
       scene
     );
+  };
+
+  const createSunlight = (scene) => {
+    const sunlight = new HemisphericLight(
+      'sunlight',
+      new Vector3(0, 1, 0),
+      scene
+    );
+    sunlight.intensity = 0.7;
   };
 
   return null;

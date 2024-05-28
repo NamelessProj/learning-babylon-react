9d37fbe6de582c9432a918fdd833cf75242c0919
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-05-28 16:30:08 +0200

    chore: Refactor App component to use React hooks and improve scene initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 // App.jsx
+import React, { useState, useCallback } from 'react';
 import { SceneComponent } from './components/SceneComponent';
+import { ArcRotateCamera, Vector3 } from '@babylonjs/core';
 import Shell from './components/Shell';
 import './App.css';
 
 const App = () => {
-  const onSceneReady = (scene) => {
-    // Qui puoi eseguire altre operazioni di configurazione della scena, se necessario
-  };
+  const [sceneState, setSceneState] = useState(null);
+  const [engineState, setEngineState] = useState(null);
 
-  const onRender = (scene) => {
-    // Qui puoi eseguire operazioni di rendering personalizzate, se necessario
-  };
+  const onSceneReady = useCallback((scene) => {
+    const engine = scene.getEngine();
+    setSceneState(scene);
+    setEngineState(engine);
+    console.log('Scene is ready');
+
+    const camera = new ArcRotateCamera(
+      'camera',
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      new Vector3(0, 5, -20),
+      scene
+    );
+    camera.setTarget(Vector3.Zero());
+    camera.attachControl(scene.getEngine().getRenderingCanvas(), true);
+  }, []);
+
+  const onRender = useCallback((scene) => {
+    // Logica di rendering
+  }, []);
 
   return (
     <SceneComponent
       antialias
       onSceneReady={onSceneReady}
       onRender={onRender}
+      id="my-canvas"
     >
-      <Shell />
+      {sceneState && engineState && (
+        <Shell scene={sceneState} engine={engineState} />
+      )}
     </SceneComponent>
   );
 };

76075b80f0b96b968839395040c97bfac111dfb2
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-06-04 16:33:33 +0200

    feat: Add InputController component for handling user input in the scene

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,25 +1,29 @@
 // Shell.jsx
-import React, { useEffect } from 'react';
-import { GameObjectContext, GameObject } from '../contexts/GameObjectContext';
+import React, { useEffect, useState } from 'react';
+import { GameObject } from '../contexts/GameObjectContext';
 import CameraController from './CameraController';
 import EnvironmentController from './EnvironnementController';
 import PlayerController from './PlayerController';
+import InputController from './InputController';
 
 const Shell = ({ scene, engine }) => {
-  useEffect(() => {
-    if (scene) {
-      scene.collisionsEnabled = true; // Aggiungi questa riga
-    }
-  }, [scene]);
+  const [inputValues, setInputValues] = useState({
+    horizontal: 0,
+    vertical: 0,
+    jumpKeyDown: false,
+  });
+
+  const handleInputUpdated = (updatedValues) => {
+    setInputValues(updatedValues);
+  };
 
   return (
-    <GameObjectContext.Provider value={{ scene, engine }}>
-      <GameObject scene={scene} engine={engine}>
-        <CameraController />
-        <EnvironmentController />
-        <PlayerController />
-      </GameObject>
-    </GameObjectContext.Provider>
+    <GameObject scene={scene} engine={engine}>
+      <CameraController />
+      <EnvironmentController />
+      <PlayerController input={inputValues} />
+      <InputController onUpdate={handleInputUpdated} />
+    </GameObject>
   );
 };
 

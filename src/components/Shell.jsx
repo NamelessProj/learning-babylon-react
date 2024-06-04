c27eb4a6bab6c6e0dce4b01cb94f2c5517bd939e
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-06-04 11:00:09 +0200

    feat: Add player controller to Shell component

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,14 +1,23 @@
 // Shell.jsx
+import React, { useEffect } from 'react';
 import { GameObjectContext, GameObject } from '../contexts/GameObjectContext';
 import CameraController from './CameraController';
 import EnvironmentController from './EnvironnementController';
+import PlayerController from './PlayerController';
 
 const Shell = ({ scene, engine }) => {
+  useEffect(() => {
+    if (scene) {
+      scene.collisionsEnabled = true; // Aggiungi questa riga
+    }
+  }, [scene]);
+
   return (
     <GameObjectContext.Provider value={{ scene, engine }}>
       <GameObject scene={scene} engine={engine}>
         <CameraController />
         <EnvironmentController />
+        <PlayerController />
       </GameObject>
     </GameObjectContext.Provider>
   );

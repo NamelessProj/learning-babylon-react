d4099f17d8c02a1d7818fa532b999d340a79cfc0
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-06-04 16:33:19 +0200

    feat: Refactor PlayerController component to use React hooks and improve player movement

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -1,11 +1,11 @@
 // PlayerController.jsx
-import React, { useEffect, useRef } from 'react';
-import { MeshBuilder, TransformNode, Vector3 } from '@babylonjs/core';
+import React, { useEffect, useRef, useContext } from 'react';
+import { MeshBuilder, TransformNode, Vector3, Scalar } from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 import { GRAVITY, PLAYER_SPEED, PLAYER_JUMP_FORCE } from '../settings/const';
 
-const PlayerController = () => {
-  const { scene } = React.useContext(GameObjectContext);
+const PlayerController = ({ input = {} }) => {
+  const { scene, engine, beforeLoop } = useContext(GameObjectContext);
   const playerRef = useRef(null);
   const velocityRef = useRef(Vector3.Zero());
 
@@ -18,7 +18,7 @@ const PlayerController = () => {
 
   const createPlayer = (scene) => {
     const body = MeshBuilder.CreateSphere("player", { diameter: 2 }, scene);
-    body.position = new Vector3(0, 5, 0); // Posiziona il player a un'altezza iniziale
+    body.position = new Vector3(0, 40, 0); // Posiziona il player a un'altezza iniziale
     body.checkCollisions = true;
     body.isPickable = false;
     body.ellipsoid = new Vector3(1, 1, 1);
@@ -29,20 +29,31 @@ const PlayerController = () => {
   useEffect(() => {
     const updatePlayer = () => {
       if (playerRef.current) {
-        // Applica la gravità alla velocità del player
-        velocityRef.current.y += GRAVITY * scene.getEngine().getDeltaTime() / 1000;
+        const { horizontal = 0, vertical = 0, jumpKeyDown = false } = input;
 
-        // Aggiorna la posizione del player in base alla velocità
+        // Update the player's horizontal velocity based on the input
+        velocityRef.current.x = horizontal * PLAYER_SPEED;
+
+        // Update the player's vertical velocity based on the input
+        velocityRef.current.z = -vertical * PLAYER_SPEED;
+
+        // Apply the jump force if the player is on the ground
+        if (jumpKeyDown && playerRef.current.position.y === 0) {
+          velocityRef.current.y = PLAYER_JUMP_FORCE;
+        }
+
+        const newVelocityY = velocityRef.current.y + GRAVITY * engine.getDeltaTime() / 1000;
+        velocityRef.current.y = Scalar.Lerp(velocityRef.current.y, newVelocityY, 0.1);
         playerRef.current.moveWithCollisions(velocityRef.current);
       }
     };
 
-    scene.registerBeforeRender(updatePlayer);
+    beforeLoop.current = updatePlayer;
 
     return () => {
-      scene.unregisterBeforeRender(updatePlayer);
+      beforeLoop.current = null;
     };
-  }, [scene]);
+  }, [engine, beforeLoop, input]);
 
   return null;
 };

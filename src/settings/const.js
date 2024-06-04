62eb3f4bc35d2a106e7ebe3131d72ac007406614
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-06-04 22:54:05 +0200

    refactor(const): Update PLAYER_SPEED constant to 0.5 for smoother player movement

--- src/settings/const.js
+++ src/settings/const.js
@@ -1,4 +1,4 @@
 // constants.js
 export const GRAVITY = -5.81; // Accelerazione di gravità in m/s^2
-export const PLAYER_SPEED = 5.0; // Velocità del player in unità/s
+export const PLAYER_SPEED = 0.5; // Velocità del player in unità/s
 export const PLAYER_JUMP_FORCE = 10.0; // Forza di salto del player in m/s^2

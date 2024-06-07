87c4cdf807a104a536f1530c96b41cf9c3972e6c
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2024-06-07 09:08:00 +0200

    feat(camera-controller): ppdate CameraController to use FollowCamera for player tracking

--- src/components/CameraController.jsx
+++ src/components/CameraController.jsx
@@ -1,16 +1,65 @@
 // CameraController.jsx
-import { useContext, useEffect } from 'react';
-import { ArcRotateCamera, Vector3 } from '@babylonjs/core';
+import { useContext, useEffect, useState } from 'react';
+import { ArcRotateCamera, Vector3, FollowCamera } from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 
 const CameraController = () => {
-  const { scene } = useContext(GameObjectContext);
+  const { scene, engine, beforeLoop } = useContext(GameObjectContext);
+  const [playerReady, setPlayerReady] = useState(false);
+
   useEffect(() => {
     if (scene) {
-      const camera = new ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0), scene);
-      camera.attachControl(scene.getEngine().getRenderingCanvas(), true);
+      const arcCamera = new ArcRotateCamera('arcCamera', -Math.PI / 2, Math.PI / 2.5, 15, new Vector3(0, 0, 0), scene);
+      arcCamera.attachControl(scene.getEngine().getRenderingCanvas(), true);
     }
   }, [scene]);
+
+  useEffect(() => {
+    let interval;
+
+    if (scene) {
+      interval = setInterval(() => {
+        const player = scene.getMeshByName("player");
+        if (player) {
+          setPlayerReady(true);
+          clearInterval(interval);
+        }
+      }, 100);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [scene]);
+
+  useEffect(() => {
+    if (scene && playerReady) {
+      const arcCamera = scene.getCameraByName('arcCamera');
+      if (arcCamera) {
+        arcCamera.dispose();
+      }
+
+      const player = scene.getMeshByName("player");
+      if (player) {
+        const followCamera = new FollowCamera("FollowCamera", new Vector3(10, 0, 10), scene);
+        followCamera.radius = 20;
+        followCamera.heightOffset = 2;
+        followCamera.rotationOffset = 180;
+        followCamera.cameraAcceleration = 0.2;
+        followCamera.maxCameraSpeed = 10;
+        followCamera.lockedTarget = player;
+
+        scene.activeCamera = followCamera;
+        followCamera.attachControl();
+
+        followCamera.dampingFactorX = 0.1;
+      followCamera.dampingFactorY = 0.1;
+      followCamera.dampingFactorZ = 0.1;
+      }
+    }
+  }, [scene, playerReady]);
 
   return null;
 };

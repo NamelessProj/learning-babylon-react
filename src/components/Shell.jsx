fab5e89ef51a11f10219c2f7ce47c76ef542402a
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2025-04-23 10:48:48 +0200

    Refactor Shell component: streamline imports, enhance player creation logic, and conditionally render CameraController

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,30 +1,32 @@
-// Shell.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GameObject } from '../contexts/GameObjectContext';
-import CameraController from './CameraController';
-import EnvironmentController from './EnvironnementController';
-import PlayerController from './PlayerController';
-import InputController from './InputController';
+import {CameraController} from './CameraController';
+import {EnvironmentController} from './EnvironnementController';
+import {PlayerController} from './PlayerController';
+import {InputController} from './InputController';
 
-const Shell = ({ scene, engine }) => {
+export const Shell = ({ scene, engine }) => {
   const [inputValues, setInputValues] = useState({
     horizontal: 0,
     vertical: 0,
     jumpKeyDown: false,
   });
+  const [playerMesh, setPlayerMesh] = useState(null);
 
   const handleInputUpdated = (updatedValues) => {
     setInputValues(updatedValues);
   };
 
+  const handlePlayerCreated = (mesh) => {
+    setPlayerMesh(mesh);
+  };
+
   return (
     <GameObject scene={scene} engine={engine}>
-      <CameraController />
       <EnvironmentController />
-      <PlayerController input={inputValues} />
+      <PlayerController input={inputValues} onPlayerCreated={handlePlayerCreated} />
+      {playerMesh && <CameraController playerMesh={playerMesh} />}
       <InputController onUpdate={handleInputUpdated} />
     </GameObject>
   );
 };
-
-export default Shell;

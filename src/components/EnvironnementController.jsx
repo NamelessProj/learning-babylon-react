0f4f61d04ac90d1f544261cd24f9a48b0fd56a17
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2025-04-23 10:48:29 +0200

    Refactor EnvironmentController: remove maze logic and unused function, streamline scene setup

--- src/components/EnvironnementController.jsx
+++ src/components/EnvironnementController.jsx
@@ -3,52 +3,16 @@ import React, { useEffect } from 'react';
 import { MeshBuilder, HemisphericLight, Vector3 } from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 
-const EnvironmentController = () => {
+export const EnvironmentController = () => {
   const { scene } = React.useContext(GameObjectContext);
-
-  const maze = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0],
-    [1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1],
-    [1,1,0,0,0,1,1,0,1,0,1,1,0,1,1,0,1,0,1,1],
-    [1,1,1,1,0,1,0,0,1,0,1,1,0,0,1,0,1,0,1,1],
-    [1,0,0,0,0,0,0,1,1,0,0,1,1,0,1,0,1,0,1,1],
-    [1,0,1,1,1,1,1,1,0,0,1,1,1,0,1,0,1,0,0,1],
-    [1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,1,0,1],
-    [1,0,0,0,0,1,1,0,1,1,0,1,1,1,1,0,1,1,1,1],
-    [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1],
-    [1,0,1,0,0,0,1,1,1,0,0,1,1,1,0,1,1,1,0,0],
-    [1,0,1,0,1,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,1,1,0,1,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,1,1,0,1,1,0,1,0,1,0,0,0,1,0,1],
-    [1,0,0,0,1,0,1,0,1,1,0,1,0,1,1,1,1,1,0,1],
-    [1,1,1,0,1,0,1,0,1,1,0,1,0,0,0,0,0,0,0,1],
-    [1,1,1,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1],
-    [1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1],
-    [1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1],
-    [1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1]
-  ];
 
   useEffect(() => {
     if (scene) {
       createGround(scene);
-      createMaze(scene)
       createSunlight(scene);
     }
   }, [scene]);
 
-  function createMaze(scene) {
-    maze.forEach((rows, z) => rows.forEach((isWall, x) => {
-      if (!isWall) return;
-      const wall = MeshBuilder.CreateBox("wall", {
-        size: 4,
-      }, scene);
-      wall.position.x = (x * 4) - 38;
-      wall.position.z = (z * 4) - 38;
-      wall.position.y = 2;
-      wall.checkCollisions = true;
-    }))
-  }
 
   const createGround = (scene) => {
     const ground = MeshBuilder.CreateGround(
@@ -70,5 +34,3 @@ const EnvironmentController = () => {
 
   return null;
 };
-
-export default EnvironmentController;
